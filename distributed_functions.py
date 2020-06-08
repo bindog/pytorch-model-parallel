81a42c1a95b709278db9701a9522b98e597b5577
bindog <bindog@outlook.com>
2020-06-08 03:06:45 -0400

    fix all gather backward bug

--- distributed_functions.py
+++ distributed_functions.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.distributed as dist
 from torch.autograd import Function
+from torch.distributed import ReduceOp
 
 
 class AllGatherFunc(Function):
@@ -14,7 +15,14 @@ class AllGatherFunc(Function):
     @staticmethod
     def backward(ctx, *grads):
         grad_list = list(grads)
-        grad_out = grad_list[dist.get_rank()]
+        rank = dist.get_rank()
+        grad_out = grad_list[rank]
+        dist_ops = [
+                dist.reduce(grad_out, rank, ReduceOp.SUM, async_op=True) if i == rank else
+                dist.reduce(grad_list[i], i, ReduceOp.SUM, async_op=True) for i in range(dist.get_world_size())
+        ]
+        for op in dist_ops:
+            op.wait()
         return (grad_out, *[None for _ in range(len(grad_list))])
 
 AllGather = AllGatherFunc.apply
